83187541d45e8c073b0873ae296ad15d514c8aa0
murilodcosta <murilodiasdacosta@gmail.com>
2026-05-13 21:12:01 -0300

    feat: creating initial CRUD and tests in postman (status - OK)

--- src/main/java/dev/murilodcosta/mastersys/repository/AlunoRepository.java
+++ src/main/java/dev/murilodcosta/mastersys/repository/AlunoRepository.java
@@ -4,4 +4,5 @@ import dev.murilodcosta.mastersys.domain.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    boolean existsByEmail(String email);
 }
---
+++ src/main/java/dev/murilodcosta/mastersys/service/AlunoService.java
@@ -0,0 +1,55 @@
+package dev.murilodcosta.mastersys.service;
+
+import dev.murilodcosta.mastersys.domain.Aluno;
+import dev.murilodcosta.mastersys.dto.AlunoRequest;
+import dev.murilodcosta.mastersys.dto.AlunoResponse;
+import dev.murilodcosta.mastersys.repository.AlunoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlunoService {
+
+    public final AlunoRepository alunoRepository;
+
+    public AlunoService(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
+    }
+
+    public AlunoResponse cadastrar(AlunoRequest alunoRequest) {
+        if (alunoRequest.email() != null && alunoRepository.existsByEmail(alunoRequest.email())) {
+            throw new RuntimeException("Já existe um aluno cadastrado com esse email");
+        }
+        var aluno = alunoRequest.toEntity();
+        var alunoSalvo = alunoRepository.save(aluno);
+        return AlunoResponse.fromEntity(alunoSalvo);
+    }
+
+    public Page<AlunoResponse> listar(Pageable pageable) {
+        var alunos = alunoRepository.findAll(pageable);
+        return alunos.map(AlunoResponse::fromEntity);
+    }
+
+    public AlunoResponse buscarPorId(Long id) {
+        var aluno = buscarAlunoPorId(id);
+        return AlunoResponse.fromEntity(aluno);
+    }
+
+    public AlunoResponse atualizar(Long id, AlunoRequest alunoRequest) {
+        var aluno = buscarAlunoPorId(id);
+        alunoRequest.preencher(aluno);
+        var alunoAtualizado = alunoRepository.save(aluno);
+        return AlunoResponse.fromEntity(alunoAtualizado);
+    }
+
+    public void excluir(Long id) {
+        var aluno = buscarAlunoPorId(id);
+        alunoRepository.delete(aluno);
+    }
+
+    private Aluno buscarAlunoPorId(Long id) {
+        return alunoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+    }
+}
